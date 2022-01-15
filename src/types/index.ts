ee5c51ca6077bd964493c8a1177e7281ae77c8d6
Mannan-Goyal <mannang6@gmail.com>
2022-01-15 17:45:52 +0530

    feat: implemented valorant authentication

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
+// For available regions
 export enum region {
     ap = 'ap',
     na = 'na',
@@ -5,10 +6,18 @@ export enum region {
     kr = 'kr',
 }
 
+// Used in /models/user.model.ts for defining schema
 export interface User {
     username: string;
     password: string;
     discord_id: Number;
     region: region;
     reminders?: string[];
+}
+
+// Used in /api/auth.ts for returning Headers object
+export interface Headers {
+    'X-Riot-Entitlements-JWT': string;
+    'X-Riot-ClientPlatform': string;
+    'X-Riot-ClientVersion': string;
 }
