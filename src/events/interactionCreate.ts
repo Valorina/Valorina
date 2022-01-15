682e5be7bd9b771416e12cd69cdfb1e07ad96117
sanjaybaskaran01 <72266283+sanjaybaskaran01@users.noreply.github.com>
2022-01-16 01:04:38 +0530

    feat: now re-deploys commands when bot starts

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,8 +1,8 @@
-import { Client, CommandInteraction } from 'discord.js';
+import { Client, Interaction } from 'discord.js';
 import { exceptionEmbed } from '../lib/embeds';
 export default {
     name: 'interactionCreate',
-    async execute(interaction: CommandInteraction, client: Client) {
+    async execute(interaction: Interaction, client: Client) {
         if (!interaction.isCommand()) return;
 
         const { default: command } = client.commands.get(interaction.commandName);
@@ -12,7 +12,7 @@ export default {
         try {
             await command.execute(interaction);
         } catch (error) {
-            await interaction.channel?.send({ embeds: [exceptionEmbed()] });
+            await interaction.reply({ embeds: [exceptionEmbed()] });
         }
     },
 };
---
+++ src/services/deployCommands.ts
@@ -0,0 +1,25 @@
+import '../lib/env';
+import fs from 'fs';
+import { REST } from '@discordjs/rest';
+import { Routes } from 'discord-api-types/v9';
+
+export const deployCommands = async (TOKEN: string, clientId: string, guildId?: string): Promise<void> => {
+    const commands: string[] = [];
+    const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const command = require(`../commands/${file}`);
+        commands.push(command.default.data.toJSON());
+        console.log(`${command.default.data['name']} ✅`);
+    }
+
+    const rest = new REST({ version: '9' }).setToken(TOKEN);
+    try {
+        console.log('Started refreshing application (/) commands.');
+
+        await rest.put(Routes.applicationGuildCommands(clientId, guildId!), { body: commands });
+
+        console.log('Successfully reloaded application (/) commands.');
+    } catch (error) {
+        console.error(error);
+    }
+};
