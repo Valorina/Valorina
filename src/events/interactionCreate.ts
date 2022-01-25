c3ab5c3fc324bd08f6c9a56e7366d552d315e152
sanjaybaskaran01 <72266283+sanjaybaskaran01@users.noreply.github.com>
2022-01-25 16:55:42 +0530

    fix: exception embed nows edits if already replied

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -12,6 +12,10 @@ export default {
         try {
             await command.execute(interaction);
         } catch (error) {
+            if (interaction.replied) {
+                await interaction.editReply({ embeds: [exceptionEmbed()] });
+                return;
+            }
             await interaction.reply({ embeds: [exceptionEmbed()] });
         }
     },
